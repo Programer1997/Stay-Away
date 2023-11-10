48e94df0bd7da76108983086bbadbf8ca7074204
Konstantin Ivanovic <121982642+Konstantin907@users.noreply.github.com>
2023-11-10 10:53:47 -0800

    some css added and logout button for cleaning full session

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,30 +1,68 @@
 import { useContext } from "react";
 import "./navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
+
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, dispatch } = useContext(AuthContext);
+
+
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+  
 
   return (
     <div className="navbar">
-      <div className="navContainer">
-        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-          <span className="logo">StayAway</span>
-        </Link>
-        {user ? (
-          user.username
-        ) : (
-          <div className="navItems">
-            <Link to="/register">
-              <button className="navButton">Register</button>
-            </Link>
-            <Link to="/login">
-              <button className="navButton">Login</button>
-            </Link>
-          </div>
-        )}
-      </div>
+    <div className="navContainer">
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+        <span className="logo">StayAway</span>
+      </Link>
+      {user ? (
+        <>
+          <span className="username">{user.username}</span>
+          <button className="logoutBtn" onClick={handleLogout}>Log out</button>
+        </>
+      ) : (
+        <div className="navItems">
+          <Link to="/register">
+            <button className="navButton">Register</button>
+          </Link>
+          <Link to="/login">
+            <button className="navButton">Login</button>
+          </Link>
+        </div>
+      )}
     </div>
+  </div>
+
+
+
+
+
+
+    // <div className="navbar">
+    //   <div className="navContainer">
+    //     <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+    //       <span className="logo">StayAway</span>
+    //       <button className="logoutBtn">Log out</button>
+    //     </Link>
+    //     {user ? (
+    //       user.username
+    //     ) : (
+    //       <div className="navItems">
+    //         <Link to="/register">
+    //           <button className="navButton">Register</button>
+    //         </Link>
+    //           <button className="navButton" onClick={handleLogout}>Login</button>
+   
+    //       </div>
+    //     )}
+    //   </div>
+    // </div>
   );
 };
 

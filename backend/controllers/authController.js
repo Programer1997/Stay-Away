d4881a5da12f927f27e327a8445c5f48d78c404d
luis abraham velazquez angulo <luisabraham.velazquez21@gmail.com>
2023-11-23 17:49:19 -0500

    updated save on database userModel property changed email to lowerCase

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,10 +9,12 @@ export const register = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
+    const emailToLowerCase = req.body.email;
 
     const newUser = new User({
       ...req.body,
       password: hash,
+      email:emailToLowerCase.toLowerCase(),
     });
 
     await newUser.save();

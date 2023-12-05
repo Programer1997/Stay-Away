36a290ed19c578786da1c47f45a0f5195f1f7253
Konstantin Ivanovic <121982642+Konstantin907@users.noreply.github.com>
2023-12-04 18:51:03 -0800

    footer changes

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -15,22 +15,20 @@ const Footer = () => {
         {/* second */}
 
         <ul className="footerList">
-            <li className="footerListItems">Countries</li>
-            <li className="footerListItems">Regions</li>
-            <li className="footerListItems">Cities</li>
-            <li className="footerListItems">Districts</li>
-            <li className="footerListItems">Airports</li>
-            <li className="footerListItems">Hotels</li>
+            <li className="footerListItems">Members</li>
+            <li className="footerListItems">Offers</li>
+            <li className="footerListItems">Discounts</li>
+            <li className="footerListItems">Deals</li>
+           
         </ul>
         {/* third */}
 
         <ul className="footerList">
-            <li className="footerListItems">Countries</li>
-            <li className="footerListItems">Regions</li>
-            <li className="footerListItems">Cities</li>
-            <li className="footerListItems">Districts</li>
-            <li className="footerListItems">Airports</li>
-            <li className="footerListItems">Hotels</li>
+            <li className="footerListItems">Monumets</li>
+            <li className="footerListItems">Must-visit</li>
+            <li className="footerListItems">Top places</li>
+            <li className="footerListItems">Nightlife</li>
+            <li className="footerListItems">Points of interests</li>
         </ul>
       </div>
       <div className="footerText">Copyright &copy; 2023</div>

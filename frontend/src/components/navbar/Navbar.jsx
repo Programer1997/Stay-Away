b4ff0d1bfa7e5986d8714e165c9df1b06b79b63f
luis abraham velazquez angulo <luisabraham.velazquez21@gmail.com>
2023-12-09 07:02:59 -0500

    frontend merging profile(franciscoTest branhc) to Application, navigate added to home page to navigate beetween profile, added dropMenu update funcionality on navBar logout added to menu also

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,19 +1,37 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./navbar.css";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 
 const Navbar = () => {
+  const [dropMenuOpen, setDropMenuOpen] = useState(false);
   const { user, dispatch } = useContext(AuthContext);
   const navigate = useNavigate();
   const handleLogout = () => {
+    setDropMenuOpen(false);
     dispatch({ type: "LOGOUT" });
     localStorage.removeItem("user");
     navigate("/login");
   };
 
+  const handleDropMenu = () => {
+    setDropMenuOpen(true);
+  };
+
+  const menuProfile = () => {
+    setDropMenuOpen(false);
+    navigate(`/profile/${user.details._id}`);
+  };
+  const handleClickOutside = (event) => {
+    if (dropMenuOpen === true) {
+      if (event.target.closest(".dropMenuContainer") === null) {
+        setDropMenuOpen(false);
+      }
+    }
+  };
+
   return (
-    <div className="navbar">
+    <div className="navbar" onClick={handleClickOutside}>
       <div className="navContainer">
         <Link to="/" style={{ color: "white", textDecoration: "none" }}>
           <span className="logo">StayAway</span>
@@ -35,10 +53,26 @@ const Navbar = () => {
                   <span>&#10132;</span>
                 </p>
               ) : null}
-              <button className="logoutBtn" onClick={handleLogout}>
+              {/*<button className="logoutBtn" onClick={handleLogout}>
                 Log out
-              </button>
+              </button>*/}
+              <img
+                src={process.env.PUBLIC_URL + "/images/client.png"}
+                alt="client charging..."
+                className="imgNavBar"
+                onClick={handleDropMenu}
+              />
             </div>
+            {dropMenuOpen ? (
+              <div className="dropMenuContainer">
+                <p className="logoutText" onClick={handleLogout}>
+                  Logout
+                </p>
+                <p className="profileText" onClick={menuProfile}>
+                  Profile
+                </p>
+              </div>
+            ) : null}
           </>
         ) : (
           <div className="navItems">

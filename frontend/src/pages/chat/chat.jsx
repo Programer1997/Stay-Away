1bc7f3939e3a5acaacd40e2ca2142d4e745bb6b0
Franciscom15 <franciscomurcia2@gmail.com>
2023-12-13 02:11:02 +0500

    added chat functionality

--- frontend/src/pages/chat/chat.jsx
+++ frontend/src/pages/chat/chat.jsx
@@ -1,37 +1,85 @@
-import React from 'react';
 import './chat.css';
 import ProfileNav from '../../components/profileNav/ProfileNav';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
 const ChatPage = () => {
+    const [message, setMessage] = useState('');
+    const [currentUserId, setCurrentUserId] = useState('')
+    const [messages, setMessages] = useState([]);
+    const [socket, setSocket] = useState(null);
+
+
+
+    useEffect(() => {
+        const newSocket = io('http://localhost:8000');
+        setSocket(newSocket);
+
+        newSocket.on('connect', () => {
+            newSocket.emit('request_my_id');
+        });
+
+        newSocket.on('my_id', (id) => {
+            setCurrentUserId(id);
+        });
+
+        newSocket.on('receive_message', (data) => {
+            setMessages(prev => [...prev, data]);
+        });
+
+        return () => newSocket.disconnect();
+    }, []);
+
+    const sendMessage = () => {
+        if (socket) {
+            socket.emit('send_message', { message });
+            setMessage('');
+        }
+    };
+
+    const formatTimestamp = (isoString) => {
+        const date = new Date(isoString);
+        return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
+    };
+
     return (
         <>
             <ProfileNav />
             <div className="chatPage">
                 <div className="chatContainer">
-                    <div className="chatSidebar">
-                        <div className="chatContact">Momma</div>
+                    {/* <div className="chatSidebar"> */}
+                        {/* Static contacts - can be made dynamic if needed */}
+                        {/* <div className="chatContact">Momma</div>
                         <div className="chatContact">Daddy</div>
-                    </div>
+                    </div> */}
                     <div className="chatMain">
                         <div className="chatHeader">
-                            Francisco Murcia
+                            Chatroom
                         </div>
                         <div className="chatMessages">
-                            <div className="message">Hello there!</div>
-                            <div className="message-container">
-                                <div className="my-message">Hey! I'm good, wby?</div>
-                            </div>
+                            {messages.map((msg, index) => (
+                                <div key={index} className={`message-container ${msg.senderId === currentUserId ? 'my-message' : 'other-message'}`}>
+                                    <div className="message">
+                                        {msg.message}
+                                        <div className="timestamp">{formatTimestamp(msg.timestamp)}</div>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                         <div className="chatInputArea">
-                            <input type="text" placeholder="Type a message..." />
-                            <button>Send</button>
+                            <input
+                                type="text"
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                placeholder="Type a message..."
+                            />
+                            <button onClick={sendMessage}>Send</button>
                         </div>
                     </div>
-
                 </div>
             </div>
         </>
     );
-}
+};
 
 export default ChatPage;
